7ff1c9678067fb3c08a307699ebbcf1cd6e260cf
Zhangrenhe <renhe.zhang@hotmail.com>
2025-09-03 17:15:50 +0800

    Add files via upload

---
+++ Algorithm/双指针/字符串.cpp
@@ -0,0 +1,19 @@
+// 字符串
+// https://ac.nowcoder.com/acm/problem/18386
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+    string str;
+    cin >> str;
+
+   
+
+    return 0;
+}

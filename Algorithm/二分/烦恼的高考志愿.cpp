7b7dce11b323a8dcd1438bf841d7584bbcd0c57f
Renhe Zhang <renhezhang@example.com>
2025-09-14 10:36:55 +0800

    9-14

--- Algorithm/二分/烦恼的高考志愿.cpp
+++ Algorithm/二分/烦恼的高考志愿.cpp
@@ -1,79 +1,58 @@
 // 烦恼的高考志愿
-// https://www.luogu.com.cn/problem/P1102
-
+// https://www.luogu.com.cn/problem/P1678
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-typedef long long ll;
 int main()
 {
-    ll m, n;
+    int m, n;
     cin >> m >> n;
-    vector<ll> school(m + 1);
+    vector<int> school(m + 1);
+    school[0] = -1e5 + 10;
     for(int i = 1; i <= m; i++)
     {
         cin >> school[i];
     }
     sort(school.begin() + 1, school.end());
+    school.push_back(1e5 + 10);
 
-
-    ll ret = 0;
-    for(ll i = 1; i <= n; i++)
+    int ret = 0;
+    for(int i = 1; i <= n; i++)
     {
-
-        
-        ll score = 0;
+        int score = 0;
         cin >> score;
-
-
-
-        // 二段性，左侧都是比score小的，右边都是比score大的，然后看左右哪个与score的差值更小
-        ll left = 1, right = m;
-        ll mid = 0;
-        while(left < right)
-        {   
+        // 特判：分数低于所有学校
+        if(score <= school[1]) 
+        {
+            ret += school[1] - score;
+            continue;
+        }
+        // 特判：分数高于所有学校
+        if(score >= school[m]) 
+        {
+            ret += score - school[m];
+            continue;
+        }
+        // 二分查找最近的两个学校分数
+        int left = 1, right = m;
+        int mid = 0;
+        while(left < right) 
+        {
             mid = (left + right) / 2;
-            if(school[mid] < score)
+            if(school[mid] < score) 
             {
                 left = mid + 1;
-            }
-            else
+            } else 
             {
                 right = mid;
             }
         }
-        if(school[left] > score)
-        {
-            left--;
-        }
-        ll retleft = left;
-
-
-
-        left = 1, right = m;
-        mid = 0;
-        while(left < right)
-        {
-            mid = (left + right + 1) / 2;
-            if(school[mid] > score)
-            {
-                right = mid - 1;
-            }
-            else
-            {
-                left = mid;
-            }
-        }
-        if(school[left] < score)
-        {
-            right++;
-        }
-        ll retright = right;
-
-
-        ret += min((score - school[retleft]), (school[retright] - score));
+        // left为第一个>=score的学校
+        int upper = left;
+        int lower = left - 1;
+        ret += min(abs(score - school[lower]), abs(school[upper] - score));
     }
 
     cout << ret << endl;

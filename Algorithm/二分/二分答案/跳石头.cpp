44de5295205320d8fac9d0211a0c91f5ce613370
Renhe Zhang <renhezhang@example.com>
2025-09-14 15:04:26 +0800

    9-14

--- Algorithm/二分/二分答案/跳石头.cpp
+++ Algorithm/二分/二分答案/跳石头.cpp
@@ -5,6 +5,25 @@
 #include <vector>
 using namespace std;
 
+
+int calc(int mid, int n, vector<int>& len)
+{
+    int c = 0;
+    for(int i = 0; i <= n; i++)
+    {
+        int j = i + 1;
+        while(j <= n + 1 && len[j] - len[i] < mid)
+        {
+            j++;
+        }
+        c += j - i - 1;
+        i = j - 1;
+    }
+    return c;
+}
+
+
+
 int main()
 {
     int l, n, m;
@@ -16,13 +35,25 @@ int main()
         cin >> len[i];
     }
     len[n + 1] = l;
-    
+
+ 
     int left = 0, right = l;
     int mid = 0;
     while(left < right)
     {
-        mid = (left + right) / 2;
-        
+        mid = (left + right + 1) / 2;
+        if(calc(mid, n, len) <= m)
+        {
+            left = mid;
+        }
+        else
+        {
+            right = mid - 1;
+        }
     }
+
+    int ret = left;
+    cout << ret << endl;
+
     return 0;
 }
